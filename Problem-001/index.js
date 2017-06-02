fed86426231e29be9c8f5d1c1e8dae8acbfcad73
Purujit Negi <purujit.negi@gmail.com>
2017-06-02 16:05:47 +0530

    Problem 1 solution

---
+++ Problem-001/index.js
@@ -0,0 +1,10 @@
+// get the sum of multiples of 3 & 5 under 1000
+function getSumOfMultiples(z){
+	var sum = 0;
+	for (var i = 3; i <= z-1; i++){
+		sum += (i%3==0 || i%5==0) ? i : 0;
+	}
+	return sum;
+}
+
+getSumOfMultiples(1000);
